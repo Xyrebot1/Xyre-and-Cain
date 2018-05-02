431fea9a5dce5c086e012efcaa9b0720b9a3c2a6
Xyrebot1 <36053918+Xyrebot1@users.noreply.github.com>
2018-05-02 14:45:17 -0600

    Woof

--- 2DArrayGame/sketch.js
+++ 2DArrayGame/sketch.js
@@ -102,44 +102,81 @@ function displayGrid() {
         image(commonTile, x * cellSize, y * cellSize, cellSize, cellSize);
       }
       else if (grid[x][y] === 2 || grid[x][y] === "2") {
-        fill(255, 50, 50);
+        noFill();
+        noStroke();
         rect(x * cellSize, y * cellSize, cellSize, cellSize);
       }
       else if (grid[x][y] === 3 || grid[x][y] === "3") {
         image(upperTile, x * cellSize, y * cellSize, cellSize, cellSize);
+        noFill();
+        noStroke();
+        rect(x * cellSize, y * cellSize, cellSize, cellSize);
       }
       else if (grid[x][y] === 4 || grid[x][y] === "4") {
         image(lowerTile, x * cellSize, y * cellSize, cellSize, cellSize);
+        noFill();
+        noStroke();
+        rect(x * cellSize, y * cellSize, cellSize, cellSize);
       }
       else if (grid[x][y] === 5 || grid[x][y] === "5") {
         image(upperTileRightOpening, x * cellSize, y * cellSize, cellSize, cellSize);
+        noFill();
+        noStroke();
+        rect(x * cellSize, y * cellSize, cellSize, cellSize);
       }
       else if (grid[x][y] === 6 || grid[x][y] === "6") {
         image(upperTileLeftOpening, x * cellSize, y * cellSize, cellSize, cellSize);
+        noFill();
+        noStroke();
+        rect(x * cellSize, y * cellSize, cellSize, cellSize);
       }
       else if (grid[x][y] === "*") {
         image(upperTileRightCorner, x * cellSize, y * cellSize, cellSize, cellSize);
+        noFill();
+        noStroke();
+        rect(x * cellSize, y * cellSize, cellSize, cellSize);
       }
       else if (grid[x][y] === "#") {
         image(upperTileLeftCorner, x * cellSize, y * cellSize, cellSize, cellSize);
+        noFill();
+        noStroke();
+        rect(x * cellSize, y * cellSize, cellSize, cellSize);
       }
       else if (grid[x][y] === 7 || grid[x][y] === "7") {
         image(lowerTileRightOpening, x * cellSize, y * cellSize, cellSize, cellSize);
+        noFill();
+        noStroke();
+        rect(x * cellSize, y * cellSize, cellSize, cellSize);
       }
       else if (grid[x][y] === 8 || grid[x][y] === "8") {
         image(lowerTileLeftOpening, x * cellSize, y * cellSize, cellSize, cellSize);
+        noFill();
+        noStroke();
+        rect(x * cellSize, y * cellSize, cellSize, cellSize);
       }
       else if (grid[x][y] === "$") {
         image(lowerTileRightCorner, x * cellSize, y * cellSize, cellSize, cellSize);
+        noFill();
+        noStroke();
+        rect(x * cellSize, y * cellSize, cellSize, cellSize);
       }
       else if (grid[x][y] === "%") {
         image(lowerTileLeftCorner, x * cellSize, y * cellSize, cellSize, cellSize);
+        noFill();
+        noStroke();
+        rect(x * cellSize, y * cellSize, cellSize, cellSize);
       }
       else if (grid[x][y] === "|") {
         image(rightTile, x * cellSize, y * cellSize, cellSize, cellSize);
+        noFill();
+        noStroke();
+        rect(x * cellSize, y * cellSize, cellSize, cellSize);
       }
       else if (grid[x][y] === ":") {
         image(leftTile, x * cellSize, y * cellSize, cellSize, cellSize);
+        noFill();
+        noStroke();
+        rect(x * cellSize, y * cellSize, cellSize, cellSize);
       }
       else {
         fill(0, 200, 255);
